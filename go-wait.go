2f41626abe8549238323f7288b7041cfce379d5e
farm rakpong <farmza560@gmail.com>
2024-01-29 20:31:08 +0700

    start api

--- go-wait.go
+++ go-wait.go
@@ -4,30 +4,28 @@ import (
 	"fmt"
 	"github.com/Kong/go-pdk"
 	"github.com/Kong/go-pdk/server"
-	"time"
+	// "time"
 )
 
 type Config struct {
-	WaitTime int
+	// WaitTime int
 }
 
 func New() interface{} {
 	return &Config{}
 }
 
-var requests = make(map[string]time.Time)
+// var requests = make(map[string]time.Time)
 
 func (config Config) Access(kong *pdk.PDK) {
-	_ = kong.Response.SetHeader("x-wait-time", fmt.Sprintf("%d seconds", config.WaitTime))
-
-	host, _ := kong.Request.GetHost()
-	lastRequest, exists := requests[host]
-
-	if exists && time.Now().Sub(lastRequest) < time.Duration(config.WaitTime)*time.Second {
-		kong.Response.Exit(400, []byte("Maximum Requests Reached"), make(map[string][]string))
-	} else {
-		requests[host] = time.Now()
+	apiKey, _ := kong.Request.GetHeader("bbbb")
+	if apiKey == "" {
+		// ไม่พบ API key ใน Header
+		kong.Response.Exit(401, []byte("Unauthorized"), make(map[string][]string))
 	}
+
+	// ทำสิ่งที่ต้องการกับ API key ที่ได้รับ
+	fmt.Println("API Key:", apiKey)
 }
 
 func main() {
